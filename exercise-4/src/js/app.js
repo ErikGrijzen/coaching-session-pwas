c77e2fcad4248b1b5fda5d60e42bf57872e31d21
Erik Grijzen <erik.grijzen@netcentric.biz>
2016-10-10 23:02:31 +0200

    Finished exercise 3 & 4.

---
+++ exercise-4/src/js/app.js
@@ -0,0 +1,130 @@
+(function () {
+    'use strict';
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+            .register('./service-worker.js')
+            .then(function () {
+                console.log('[ServiceWorker] Registered');
+            })
+            .catch(function (error) {
+                console.log('[ServiceWorker] Registration failed:', error);
+            });
+    }
+
+    var menuBtn = document.querySelector('.header__menuBtn'),
+
+        navigationActiveClass = 'navigation-is-active',
+        talksBtn = document.querySelectorAll('.navigation__btn')[1],
+
+        main = document.querySelector('.main'),
+
+        loader = document.querySelector('.loader-circle'),
+        loaderIsHiddenClass = 'is-hidden';
+
+    function init() {
+        loadTalks();
+    }
+
+    function showLoader() {
+        loader.classList.remove(loaderIsHiddenClass);
+    }
+
+    function hideLoader() {
+        loader.classList.add(loaderIsHiddenClass);
+    }
+
+    function openMenu() {
+        document.body.classList.add(navigationActiveClass);
+    }
+
+    function closeMenu() {
+        document.body.classList.remove(navigationActiveClass);
+    }
+
+    function onMenuButtonClicked() {
+        if (document.body.classList.contains(navigationActiveClass)) {
+            closeMenu();
+        } else {
+            openMenu();
+        }
+    }
+
+    function renderTalksData(data) {
+        var list = document.createElement('ul');
+        list.className = 'talks__list';
+
+        data.talks.forEach(function (talk) {
+            var listItem = document.createElement('li');
+            listItem.className = 'talks__listItem';
+
+            var title = document.createElement('h2');
+            title.className = 'talks__title';
+            title.innerText = talk.title;
+            listItem.appendChild(title);
+
+            var speaker = document.createElement('p');
+            speaker.className = 'talks__speaker';
+            speaker.innerHTML = '<strong>By: </strong> ' + talk.speaker;
+            listItem.appendChild(speaker);
+
+            var products = document.createElement('p');
+            products.className = 'talks__category';
+            products.innerHTML = '<strong>Category: </strong>' + talk.products;
+            listItem.appendChild(products);
+
+            var targetAudience = document.createElement('p');
+            targetAudience.className = 'talks__audience';
+            targetAudience.innerHTML = '<strong>Target audience: </strong>' + talk.audience;
+            listItem.appendChild(targetAudience);
+
+            list.appendChild(listItem);
+        });
+
+        main.innerHTML = list.outerHTML;
+    }
+
+    function loadTalks() {
+        var url = 'api/talks.json';
+
+        showLoader();
+
+        if ('caches' in window) {
+            caches.match(url)
+                .then(function(response) {
+                    if (response) {
+                        console.log('[ServiceWorker] Load API data from cache');
+
+                        response.json().then(function(data) {
+                            renderTalksData(data);
+                        });
+                    }
+                });
+        }
+
+        fetch(url)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                renderTalksData(data);
+
+                hideLoader();
+            })
+            .catch(function() {
+                hideLoader();
+            });
+    }
+
+    function onTalksButtonClicked() {
+        closeMenu();
+
+        loadTalks();
+    }
+
+    menuBtn.addEventListener('click', onMenuButtonClicked);
+    talksBtn.addEventListener('click', onTalksButtonClicked);
+
+    init();
+
+})();
---
+++ exercise-4/src/service-worker.js
@@ -0,0 +1,65 @@
+var appShellCacheName = 'ncSummitAppShell-0.0.2';
+var appDataCacheName = 'ncSummitData-0.0.2';
+var appShellCacheFiles= [
+    '/',
+    '/index.html',
+    '/css/main.css',
+    '/js/app.js',
+    '/images/logo__regular.svg',
+    '/fonts/icon-font.woff',
+    '/fonts/icon-font.eot',
+    '/fonts/icon-font.svg',
+];
+
+self.addEventListener('install', function (event) {
+    console.log('[ServiceWorker] Install');
+
+    event.waitUntil(
+        caches.open(appShellCacheName).then(function (cache) {
+            console.log('[ServiceWorker] Caching app shell');
+
+            return cache.addAll(appShellCacheFiles);
+        })
+    );
+});
+
+self.addEventListener('activate', function (event) {
+    console.log('[ServiceWorker] Activate');
+
+    event.waitUntil(
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== appShellCacheName && key !== appDataCacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', function (event) {
+    console.log('[ServiceWorker] Fetch:', event.request.url);
+
+    if (event.request.url.indexOf('api') > -1) {
+        event.respondWith(
+            caches.open(appDataCacheName).then(function(cache) {
+                return fetch(event.request)
+                    .then(function(response) {
+                        cache.put(event.request.url, response.clone());
+
+                        return response;
+                    });
+            })
+        );
+    } else {
+        event.respondWith(
+            caches.match(event.request).then(function(response) {
+                return response || fetch(event.request);
+            })
+        );
+    }
+});
